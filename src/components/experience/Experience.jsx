e7111dca1c32ef374ce52aa440b625785ebc07af
Eduard Malyshenko <ed54rus@mail.ru>
2023-02-19 00:01:51 +0700

    выполнен блок experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,54 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
+
+const frontendSkills = [
+  { skill: "HTML", level: "Experienced" },
+  { skill: "CSS", level: "Experienced" },
+  { skill: "JavaScript", level: "Experienced" },
+  { skill: "Bootstrap", level: "Experienced" },
+  { skill: "Tailwind", level: "Experienced" },
+  { skill: "React", level: "Experienced" },
+];
+
+const backendSkills = [
+  { skill: "Node JS", level: "Experienced" },
+  { skill: "MongoDB", level: "Experienced" },
+  { skill: "PHP", level: "Experienced" },
+  { skill: "MySQL", level: "Experienced" },
+  { skill: "Python", level: "Experienced" },
+];
+
+const getSkills = (skills) =>
+  skills.map((item, id) => (
+    <article className="experience__details" key={id}>
+      <BsPatchCheckFill className="experience__details-icon" />
+      <div>
+        <h4>{item.skill}</h4>
+        <small className="text-light">{item.level}</small>
+      </div>
+    </article>
+  ));
 
 const Experience = () => {
   return (
-    <section id="experience"></section>
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">{getSkills(frontendSkills)}</div>
+        </div>
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">{getSkills(backendSkills)}</div>
+        </div>
+
+      </div>
+    </section>
   );
 };
 
