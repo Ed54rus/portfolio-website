864e577e793649a3b0eaac0cf7577ac65730938d
Eduard Malyshenko <ed54rus@mail.ru>
2023-02-19 12:20:08 +0700

    выполнен блок services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,76 @@
 import React from "react";
-import './services.css'
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
+
+const design = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+];
+
+const webDev = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+];
+
+const contentCreation = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+];
+
+const getServiceList = (list) =>
+  list.map((item, id) => (
+    <li key={id}>
+      <BiCheck className="service__list-icon" />
+      <p>{item}</p>
+    </li>
+  ));
 
 const Services = () => {
   return (
-    <section id="services"></section>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">{getServiceList(design)}</ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">{getServiceList(webDev)}</ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">{getServiceList(contentCreation)}</ul>
+        </article>
+      </div>
+    </section>
   );
 };
 
