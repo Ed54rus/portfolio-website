8843c43799d24b3c8649ec4877de4c5830015c92
Eduard Malyshenko <ed54rus@mail.ru>
2023-03-04 22:42:47 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import Eduard from "../../assets/me.png";
+import Eduard from "../../assets/portrait.jpg";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
